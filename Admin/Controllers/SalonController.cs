ec6349c36ceb18c526358e6c128e82c6e4bb930c
Fatih Kaya <147181335+katze2023@users.noreply.github.com>
2025-12-12 23:22:04 +0300

    Admin kullanıcı özelliklerinin bazıları eklendi

---
+++ Admin/Controllers/SalonController.cs
@@ -0,0 +1,86 @@
+﻿using FitnessCenterManagement.Data;
+using FitnessCenterManagement.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessCenterManagement.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class SalonController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SalonController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var salons = _context.Salons.ToList();
+            return View(salons);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Salon salon)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Salons.Add(salon);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(salon);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var salon = _context.Salons.Find(id);
+            if (salon == null) return NotFound();
+
+            return View(salon);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Salon salon)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Salons.Update(salon);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(salon);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var salon = _context.Salons.Find(id);
+            if (salon == null) return NotFound();
+
+            return View(salon);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var salon = _context.Salons.Find(id);
+            if (salon == null) return NotFound();
+
+            _context.Salons.Remove(salon);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Admin/Controllers/ServiceController.cs
@@ -0,0 +1,94 @@
+﻿using FitnessCenterManagement.Data;
+using FitnessCenterManagement.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessCenterManagement.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class ServiceController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ServiceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // LISTE: /Admin/Service
+        public IActionResult Index()
+        {
+            var services = _context.Services.ToList();
+            return View(services);
+        }
+
+        // CREATE GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // CREATE POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Service service)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Services.Add(service);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(service);
+        }
+
+        // EDIT GET
+        public IActionResult Edit(int id)
+        {
+            var service = _context.Services.Find(id);
+            if (service == null) return NotFound();
+
+            return View(service);
+        }
+
+        // EDIT POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Service service)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Services.Update(service);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(service);
+        }
+
+        // DELETE GET
+        public IActionResult Delete(int id)
+        {
+            var service = _context.Services.Find(id);
+            if (service == null) return NotFound();
+
+            return View(service);
+        }
+
+        // DELETE POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var service = _context.Services.Find(id);
+            if (service == null) return NotFound();
+
+            _context.Services.Remove(service);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
